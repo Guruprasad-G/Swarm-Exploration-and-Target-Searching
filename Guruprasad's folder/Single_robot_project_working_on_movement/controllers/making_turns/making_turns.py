e7e8daf12c533065a5ddac792df5c4d7260e30a7
Guruprasad-G <60037540+Guruprasad-G@users.noreply.github.com>
2021-07-06 10:07:01 +0530

    Added Comments to making_turns.py
    4 Issues.
    1) Exact Value of compass by passes if condition
    2) Approximate Compass value offsets the robot by significant angle
    3) X_pos value dosen't seem to change.
    4) Global dictionary not getting updated

--- Guruprasad's folder/Single_robot_project_working_on_movement/controllers/making_turns/making_turns.py
+++ Guruprasad's folder/Single_robot_project_working_on_movement/controllers/making_turns/making_turns.py
@@ -28,20 +28,18 @@ down_sensor.enable(timeStep)
 wheel_left = robot.getDevice("left wheel motor")
 wheel_right = robot.getDevice("right wheel motor")
 
+#Appending In-Built Sensors ---- Based on Erebus Rescue Sim
 leftSensors = []
 rightSensors = []
 frontSensors = []
-
 frontSensors.append(robot.getDevice("ps7"))
 frontSensors[0].enable(timeStep)
 frontSensors.append(robot.getDevice("ps0"))
 frontSensors[1].enable(timeStep)
-
 rightSensors.append(robot.getDevice("ps1"))
 rightSensors[0].enable(timeStep)
 rightSensors.append(robot.getDevice("ps2"))
 rightSensors[1].enable(timeStep)
-
 leftSensors.append(robot.getDevice("ps5"))
 leftSensors[0].enable(timeStep)
 leftSensors.append(robot.getDevice("ps6"))
@@ -49,24 +47,112 @@ leftSensors[1].enable(timeStep)
 
 #[left wheel speed, right wheel speed]
 speeds = [max_velocity,max_velocity]
+wheel_left.setPosition(float("inf"))
+wheel_right.setPosition(float("inf"))
 
+#Initializing Required Variables
 current_dir = []
-next_dir = []
+global_dict = {}
+time_counter = 0
 
+#Declaring Standard Variables
 right = (0.0, -0.0, 1.0)
 up = (-0.0, 1.0, -0.0)
 left = (0.0, -0.0, -1.0)
 down = (0.0, -1.0, -0.0)
-
 direction_dict = {"right":right,"up":up,"left":left,"down":down}
-
 left_next_dir =   {right:list(up), up:list(left), left:list(down), down:list(right)}
 right_next_dir =  {right:list(down), down:list(left), left:list(up), up:list(right)}
 rotate_next_dir = {right:list(left), left:list(right), up:list(down), down:list(up)}
 
-wheel_left.setPosition(float("inf"))
-wheel_right.setPosition(float("inf"))
+#Varibales required for Mid-point delay -------Still in testing, can be Bypassed 
+lst = [5120,6.28*32*2,6.28*32*3,6.28*32*4]
+starting_time = 0
+counter_dict = {0.5:0,0.4:0,0.3:0,0.2:0,0.1:0,0.0:0,-0.1:0,-0.2:0,-0.3:0,-0.4:0,-0.5:0,-0.6:0,-0.7:0,-0.8:0,-0.9:0,-1:0,
+                -1.1:0,-1.2:0,-1.3:0,-1.4:0,-1.5:0,-1.6:0,-1.7:0,-1.8:0,-1.9:0,-2.0:0,-2.1:0,-2.2:0,-2.3:0,-2.4:0,-2.5:0}
 
+#Functions
+def obstacle_finder(inp):
+    if inp>=0.5:
+        return "clear path"
+    else:
+        return "wall"
+
+def movement_decision(X_pos,Z_pos):
+    global global_dict
+    global current_dir
+    global right
+    global up
+    global left
+    global down
+    if tuple(current_dir) == right:
+        if global_dict[(X_pos,Z_pos)][0] == "wall":
+            if global_dict[(X_pos,Z_pos)][3] == "clear path":
+                return "turn_right"
+            elif global_dict[(X_pos,Z_pos)][1] == "clear path":
+                return "turn_left"
+            elif global_dict[(X_pos,Z_pos)][2] == "clear path":
+                return "turn_back"
+            else:
+                print("Hey I am Surrounded on all the sides man!!")
+                return "delay"
+        else:
+            return "forward"
+    elif tuple(current_dir) == up:
+        if global_dict[(X_pos,Z_pos)][1] == "wall":
+            if global_dict[(X_pos,Z_pos)][0] == "clear path":
+                return "turn_right"
+            elif global_dict[(X_pos,Z_pos)][2] == "clear path":
+                return "turn_left"
+            elif global_dict[(X_pos,Z_pos)][3] == "clear path":
+                return "turn_back"
+            else:
+                print("Hey I am Surrounded on all the sides man!!")
+                return "delay"
+        else:
+            return "forward"    
+    elif tuple(current_dir) == left:
+        if global_dict[(X_pos,Z_pos)][2] == "wall":
+            if global_dict[(X_pos,Z_pos)][1] == "clear path":
+                return "turn_right"
+            elif global_dict[(X_pos,Z_pos)][3] == "clear path":
+                return "turn_left"
+            elif global_dict[(X_pos,Z_pos)][0] == "clear path":
+                return "turn_back"
+            else:
+                print("Hey I am Surrounded on all the sides man!!")
+                return "delay"
+        else:
+            return "forward"
+    elif tuple(current_dir) == down:
+        if global_dict[(X_pos,Z_pos)][3] == "wall":
+            if global_dict[(X_pos,Z_pos)][2] == "clear path":
+                return "turn_right"
+            elif global_dict[(X_pos,Z_pos)][0] == "clear path":
+                return "turn_left"
+            elif global_dict[(X_pos,Z_pos)][1] == "clear path":
+                return "turn_back"
+            else:
+                print("Hey I am Surrounded on all the sides man!!")
+                return "delay"
+        else:
+            return "forward"
+    else :
+        return "forward"
+
+def move(name):
+    global current_dir
+    if name=="forward":
+        speeds[0] = max_velocity
+        speeds[1] = max_velocity
+    elif name=="turn_right":
+        turn_right()
+    elif name=="turn_left":
+        turn_left()
+    elif name=="turn_back":
+        spin()
+    else:
+        delay()
 
 def turn_right():
     global current_dir
@@ -101,123 +187,56 @@ def delay():
     speeds[0] = 0
     speeds[1] = 0
 
-def move(name):
-    global current_dir
-    if name=="forward":
-        speeds[0] = max_velocity
-        speeds[1] = max_velocity
-    elif name=="turn_right":
-        turn_right()
-    elif name=="turn_left":
-        turn_left()
-    elif name=="turn_back":
-        spin()
-    else:
-        delay()
-
-def obstacle_finder(inp):
-    if inp>=0.5:
-        return "clear path"
-    else:
-        return "wall"
-
-def movement_decision(X_pos,Z_pos):
-    global global_dict
-    global current_dir
-    global right
-    global up
-    global left
-    global down
-    if tuple(current_dir) == right and global_dict[(X_pos,Z_pos)][0] == "wall":
-        if global_dict[(X_pos,Z_pos)][3] == "clear path":
-            return "turn_right"
-        elif global_dict[(X_pos,Z_pos)][1] == "clear path":
-            return "turn_left"
-        elif global_dict[(X_pos,Z_pos)][2] == "clear path":
-            return "turn_back"
-        else:
-            print("Hey I am Surrounded on all the sides man!!")
-            return "delay"
-    elif tuple(current_dir) == up and global_dict[(X_pos,Z_pos)][1] == "wall":
-        if global_dict[(X_pos,Z_pos)][0] == "clear path":
-            return "turn_right"
-        elif global_dict[(X_pos,Z_pos)][2] == "clear path":
-            return "turn_left"
-        elif global_dict[(X_pos,Z_pos)][3] == "clear path":
-            return "turn_back"
-        else:
-            print("Hey I am Surrounded on all the sides man!!")
-            return "delay"
-    elif tuple(current_dir) == left and global_dict[(X_pos,Z_pos)][2] == "wall":
-        if global_dict[(X_pos,Z_pos)][1] == "clear path":
-            return "turn_right"
-        elif global_dict[(X_pos,Z_pos)][3] == "clear path":
-            return "turn_left"
-        elif global_dict[(X_pos,Z_pos)][0] == "clear path":
-            return "turn_back"
-        else:
-            print("Hey I am Surrounded on all the sides man!!")
-            return "delay"
-    elif tuple(current_dir) ==  down and global_dict[(X_pos,Z_pos)][3] == "wall":
-        if global_dict[(X_pos,Z_pos)][2] == "clear path":
-            return "turn_right"
-        elif global_dict[(X_pos,Z_pos)][0] == "clear path":
-            return "turn_left"
-        elif global_dict[(X_pos,Z_pos)][1] == "clear path":
-            return "turn_back"
-        else:
-            print("Hey I am Surrounded on all the sides man!!")
-            return "delay"
-    else:
-        return "forward"
-
-lst = [5120,6.28*32*2,6.28*32*3,6.28*32*4]
-time_counter = 0
-starting_time = 0
-global_dict = {}
-counter_dict = {0.5:0,0.4:0,0.3:0,0.2:0,0.1:0,0.0:0,-0.1:0,-0.2:0,-0.3:0,-0.4:0,-0.5:0,-0.6:0,-0.7:0,-0.8:0,-0.9:0,-1:0,
-                -1.1:0,-1.2:0,-1.3:0,-1.4:0,-1.5:0,-1.6:0,-1.7:0,-1.8:0,-1.9:0,-2.0:0,-2.1:0,-2.2:0,-2.3:0,-2.4:0,-2.5:0}
+#Main loop which executed at every time step
 while robot.step(timeStep) != -1:
+    #Incrementing Time counter to get current simulation time
     time_counter += timeStep
+    
+    #To get approximate GPS position of the robot
     X_pos = round(gps.getValues()[1],1)
     Z_pos = round(gps.getValues()[2],1) 
+    print("X =",X_pos,"Z =",Z_pos)
     
-    
+    #By default the robot keeps moving forward
     speeds[0] = max_velocity
     speeds[1] = max_velocity
-    up_distance = up_sensor.getValue()
-    right_distance = right_sensor.getValue()
-    left_distance = left_sensor.getValue()
-    down_distance = down_sensor.getValue()
-    priority_list = [["up",up_distance],["right",right_distance],["left",left_distance],["down",down_distance]]
-    #print("X =",X_pos,"Z =",Z_pos)
     
+    #The folowing gets executed when the robot is at the approximate center of the square
     if str(Z_pos)[-1]=='5' or str(X_pos)[-1]=='5':
+        #To get the sensor values
         right_value = right_sensor.getValue()
         up_value = up_sensor.getValue()
         left_value = left_sensor.getValue()
         down_value = down_sensor.getValue()
+        
+        #Temporary list that stores surrounding details 
         surrounding = [obstacle_finder(right_value),obstacle_finder(up_value),
                        obstacle_finder(left_value),obstacle_finder(down_value)]
+        #Updating Global data
         global_dict[(X_pos,Z_pos)] = global_dict.get((X_pos,Z_pos),surrounding)
-        #print("global_dict :",global_dict)
+        print("global_dict :",global_dict)
         
+        #To get compass value to check robot orientation
         compass_val = [round(compass.getValues()[0],1),round(compass.getValues()[1],1),round(compass.getValues()[2],1)]
-        print(compass_val)
+        print("Compass Value :",compass_val)
+        #Updates current direction value only when compass value is in right,up,left or down
         if tuple(compass_val) in [right,up,left,down]:
-            print("***************current dir set**************")
-            current_dir = compass_val 
+            print("Current dir set to",compass_val)
+            current_dir = compass_val
+        #Calls movement decision which returns how to move about 
         move(movement_decision(X_pos,Z_pos))
      
-    
+    #Receives message only when Message Queue is greater than 0
     if receiver.getQueueLength()>0:
+        #Webots way of reciveing message
         message=receiver.getData()
-        #print(message)
+        #Message needs to be unpacked to recover orginal message
         dataList=struct.unpack("i",message)
         print("Message =",dataList)
         if 1 in dataList:
             print("Second robot has detected wall on it's right side -- by First robot")
     
+    #Initializing CameraRecognitionObject
     object= camera.getRecognitionObjects()
     #print(obj)
     for item in object:
@@ -238,7 +257,7 @@ while robot.step(timeStep) != -1:
                delay()
     
     
-    
+    #Code for midpoint delay -------- Need to work on it, Can be bypassed
     if time_counter in lst:
         starting_time = time_counter
         
@@ -256,7 +275,8 @@ while robot.step(timeStep) != -1:
     #print("*******")
     #print(counter_dict)
     #print(X_pos,Z_pos) 
-
+    
+    #**********E-Puck Erebus Rescue Sim Code Copy*************#
     for i in range(2):
         #for sensors on the left, either
         if leftSensors[i].getValue() > 80:
