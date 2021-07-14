712b255c95f7a41b45d83a0b8a348bc1c7e9599e
Guruprasad-G <60037540+Guruprasad-G@users.noreply.github.com>
2021-07-14 15:39:22 +0530

    Added Complete project until now to project folder
    Added comments and commented print statements

---
+++ Guruprasad's folder/Single_robot_project_working_on_movement/controllers/mypuck_comms_only_controller/mypuck_comms_only_controller.py
@@ -0,0 +1,123 @@
+"""mypuck_comms_only_controller controller."""
+
+#Importing required library Robot class
+from controller import Robot
+#Importing struct library required to convert messages to bytes for transmission
+import struct
+#Creating instance of the Robot class to access the robot(E-puck)
+robot = Robot()
+#Getting the timestep of the world
+timeStep = int(robot.getBasicTimeStep())
+#Acessing and enabling required components present in the robot
+camera = robot.getDevice("colour_camera")
+camera.enable(timeStep)
+camera.recognitionEnable(timeStep)
+gps = robot.getDevice("gps")
+gps.enable(timeStep)
+compass = robot.getDevice("compass")
+compass.enable(timeStep)
+emitter = robot.getDevice("emitter")
+up_sensor = robot.getDevice("up_sensor")
+up_sensor.enable(timeStep)
+right_sensor = robot.getDevice("right_sensor")
+right_sensor.enable(timeStep)
+left_sensor = robot.getDevice("left_sensor")
+left_sensor.enable(timeStep)
+down_sensor = robot.getDevice("down_sensor")
+down_sensor.enable(timeStep)
+
+#Declaring standandard values (Standard Right, Up, Left and Down values got from the compass)
+right = (0.0, -0.0, 1.0)
+up = (-0.0, 1.0, -0.0)
+left = (0.0, -0.0, -1.0)
+down = (0.0, -1.0, -0.0)
+#Variable which turn True when target is detected/reached
+done = False
+#Dicticonary to store Map data
+global_dict = {}
+
+#Fucntions
+def robot_orientation(compass_val,surrounding,right_value,up_value,left_value,down_value):
+    '''This function checks which direction the robot is facing and returns the surrounding data with respect to actual(global) directions'''
+    global right
+    global up
+    global left
+    global down
+    if tuple(compass_val) == up:
+        surrounding = [obstacle_finder(right_value),obstacle_finder(up_value),
+                       obstacle_finder(left_value),obstacle_finder(down_value)]
+    elif tuple(compass_val) == right:
+        surrounding = [obstacle_finder(up_value),obstacle_finder(left_value),
+                       obstacle_finder(down_value),obstacle_finder(right_value)]
+    elif tuple(compass_val) == left:
+        surrounding = [obstacle_finder(down_value),obstacle_finder(right_value),
+                       obstacle_finder(up_value),obstacle_finder(left_value)]
+    elif tuple(compass_val) == down:
+        surrounding = [obstacle_finder(left_value),obstacle_finder(down_value),
+                       obstacle_finder(right_value),obstacle_finder(up_value)]
+    else :
+        print("Compass Value Error : Invalid Compass value inside robot_orientation function")
+    return surrounding
+
+def obstacle_finder(inp):
+    ''' Returns True if it is a Clear-path and False if there is a Wall '''
+    if inp>=0.5:
+        return True
+    else:
+        return False
+
+#Main Infinite Loop
+while robot.step(timeStep) != -1:
+    #Varible to monitor timing
+    timings = range(2,180,2)
+    #Looping through available timings
+    for index,time in enumerate(timings):
+        #This While loop to holds the robot until required time is met
+        while robot.getTime() < time:
+            if robot.step(timeStep) == -1:
+                quit()
+        #Obtaining GPS values and rounding it to 1 decimal place
+        X_pos = round(gps.getValues()[0],1)
+        Z_pos = round(gps.getValues()[2],1)
+        #print("Debug: Position :","X pos =",X_pos,"Z pos =",Z_pos)
+        #Obtaining Sensor values
+        right_value = right_sensor.getValue()
+        up_value = up_sensor.getValue()
+        left_value = left_sensor.getValue()
+        down_value = down_sensor.getValue()
+        #Obtaining Compass value to know the orientation of the robot
+        compass_val = [round(compass.getValues()[0],1),round(compass.getValues()[1],1),round(compass.getValues()[2],1)]
+        #Instamce of the recognition node
+        object= camera.getRecognitionObjects()
+        #Iterating through the recognition instance
+        for item in object:
+            #Declaring variable
+            target_pos = []
+            #Checking for required recognition colour
+            if item.get_colors()== [1,0,0]:
+                #Getting the relative position of the target from the robot
+                target_pos= item.get_position()
+                #Getting the type/model of the target
+                target_model= item.get_model()
+                #print("Debug: Relative position of target","Target_pos =",target_pos)
+                x_pos_of_target = gps.getValues()[0]+target_pos[0]
+                y_pos_of_target = gps.getValues()[1]+target_pos[1]
+                z_pos_of_target = gps.getValues()[2]+target_pos[2]
+                #Condition to check if robot is close enough to the target
+                if target_pos[0]>=0.0085:
+                    #Setting the done varibale to True since target is detected and robot is near the target
+                    done = True
+                    print("Target is at position","X =",x_pos_of_target,"Y =",y_pos_of_target,"Z =",z_pos_of_target)
+                    #print("Relative_pos =",target_pos,"Model =",target_model)
+        #Varibale to store data regarding Clear-path or Wall in all four directions
+        surrounding = []
+        #Obatain surroundings data by passign robot orientation and sensor data
+        surrounding = robot_orientation(compass_val,surrounding,right_value,up_value,left_value,down_value)
+        #Updating Global map data
+        global_dict[(X_pos,Z_pos)] = global_dict.get((X_pos,Z_pos),surrounding)
+        #Pack the message into bytes for transmission
+        message = struct.pack("? f f ? ? ? ?",done,X_pos,Z_pos,surrounding[0],surrounding[1],surrounding[2],surrounding[3])
+        print("Message =",X_pos,Z_pos,surrounding[0],surrounding[1],surrounding[2],surrounding[3])
+        #Sending the message
+        emitter.send(message)
+    #print("Global dict :",global_dict)
--- Swarm Exploration and Target searching/Version 3.2/controllers/mypuck_supervisor/mypuck_supervisor.py
+++ Swarm Exploration and Target searching/Version 3.2/controllers/mypuck_supervisor/mypuck_supervisor.py
@@ -1,16 +1,19 @@
-"""my_5_supervisor_controller controller."""
+"""mypuck_supervisor controller."""
 
 #Importing the Supervisor library
 from controller import Supervisor
 #Importing struct library required to convert bytes to message on reception of message
 import struct
+#Creating instance of the supervisor class
 supervisor = Supervisor()
-
-timestep = 32
+#Getting the timestep of the world
+timestep = int(supervisor.getBasicTimeStep())
+#Creating access to each of the four E-pucks
 robot_node1 = supervisor.getFromDef("EPUCK1")
 robot_node2 = supervisor.getFromDef("EPUCK2")
 robot_node3 = supervisor.getFromDef("EPUCK3")
 robot_node4 = supervisor.getFromDef("EPUCK4")
+#Acessing and enabling required components present in the supervisor
 receiver1 = supervisor.getDevice("receiver1")
 receiver1.enable(timestep)
 receiver2 = supervisor.getDevice("receiver2")
@@ -19,8 +22,8 @@ receiver3 = supervisor.getDevice("receiver3")
 receiver3.enable(timestep)
 receiver4 = supervisor.getDevice("receiver4")
 receiver4.enable(timestep)
-emitter = supervisor.getDevice("emitter")
 
+#Getting access to each of the robot's translation and rotation
 trans_field1 = robot_node1.getField("translation")
 rot_field1= robot_node1.getField("rotation")
 trans_field2 = robot_node2.getField("translation")
@@ -30,11 +33,27 @@ rot_field3= robot_node3.getField("rotation")
 trans_field4 = robot_node4.getField("translation")
 rot_field4= robot_node4.getField("rotation")
 
-global_map_dict = {}
+#Declaring standandard values (Standard Right, Up, Left and Down values got from the rotation values)
 right = (0,1,0,-1.57071) 
 up = (0,1,0,0)
 left = (0,1,0,1.57071)
 down = (0,1,0,3.14142)
+#Length of the Arena leaving the buffer zone
+length_of_arena = 8
+#Varibles to monitor current and next nodes of the robots
+next_node1 = 0
+current_node1 = 0
+next_node2 = 0
+current_node2 = 0
+next_node3 = 0
+current_node3 = 0
+next_node4 = 0
+current_node4 = 0
+
+#Dictionary containing Map data collected by each of the robots
+global_map_dict = {}
+#A list that is used to mark the nodes that are visited by the robots
+visited = [False for i in range(64)]
 
 #Stack code from YouTube
 class Stack():
@@ -55,16 +74,15 @@ class Stack():
     def __str__(self):
         return str(self.stack)
 
+#Creating instances pf Stack for each of the robot
 stack1 = Stack()
 stack2 = Stack()
 stack3 = Stack()
 stack4 = Stack()
-
+#Dictionary that corresponds robot to their respective stack
 stack_access_dict = {1:stack1,2:stack2,3:stack3,4:stack4}
 
 #Graph code from GeeksForGeeks
-# Python program for 
-# validation of a graph
   
 # import dictionary for graph
 from collections import defaultdict
@@ -84,55 +102,38 @@ def generate_edges(graph):
             # if edge exists then append
             edges.append((node, neighbour))
     return edges
-    
-def find_shortest_path(graph, start, end, path =[]):
-        path = path + [start]
-        if start == end:
-            return path
-        shortest = None
-        for node in graph[start]:
-            if node not in path:
-                newpath = find_shortest_path(graph, node, end, path)
-                if newpath:
-                    if not shortest or len(newpath) < len(shortest):
-                        shortest = newpath
-        return shortest  
-# declaration of graph as dictionary
-#addEdge(graph,'a','c') 
-# Driver Function call 
-# to print generated graph
-#print(generate_edges(graph))
-visited = [False for i in range(1000)]
-
 
 #Functions
 def next_node_generator(current_node,robot_number):
+    '''This function updates the nodes visited by a robot and checks the possible path it can go.'''
+    '''It also keeps a track of nodes visited for backtracking in case of deadends'''
     global visited
     global graph
     global stack_access_dict
+    #Add current node to corresponding robot stack
     stack_access_dict[robot_number].push(current_node)
+    #Update current node as visited
     visited[current_node] = True
+    #Check the connections for current node
     for node in graph[current_node]:
+        #If one of the connected node is not visited, return it
         if (not visited[node]):
-            #print("Node to be returned =",node)
+            #print("Debug: ","Node to be returned =",node)
             return node
+    #If all connected nodes are visited, pop out current node from stack
     stack_access_dict[robot_number].pop()
-    #print("Back path to be returned =",stack.peek())
+    #Return the node that was pushed into the stack before the current node
+    #print("Debug: ","Back path to be returned =",stack.peek())
     return stack_access_dict[robot_number].pop()
-    
-
 
 def message_to_map_converion(X_pos,Z_pos,r_detail,u_detail,l_detail,d_detail):
+    '''This function updates the Global dictionary containing map data based on message recived from the robots'''
     global global_map_dict
     surrounding = [r_detail,u_detail,l_detail,d_detail]
     global_map_dict[(X_pos,Z_pos)] = global_map_dict.get((X_pos,Z_pos),surrounding)
 
-left_next_dir =   {right:list(up), up:list(left), left:list(down), down:list(right)}
-right_next_dir =  {right:list(down), down:list(left), left:list(up), up:list(right)}
-rotate_next_dir = {right:list(left), left:list(right), up:list(down), down:list(up)}
-
 def graph_updation(current_node,r,u,l,d):
-    #print("Current node",current_node)
+    '''This function updates the nodes and their connection to graph data structure based on message received from the robots'''
     global graph
     if u and (current_node-8 not in graph[current_node]):
         addEdge(graph,current_node,current_node-8)
@@ -143,17 +144,8 @@ def graph_updation(current_node,r,u,l,d):
     if d and (current_node+8 not in graph[current_node]):
         addEdge(graph,current_node,current_node+8)
 
-def which_side(current_node,next_node):
-    if current_node+1 == next_node:
-        print("Going right")
-    elif current_node-8 == next_node:
-        print("Going up")
-    elif current_node-1 == next_node:
-        print("Going left")
-    elif current_node+8 == next_node:
-        print("Going down")
-
 def next_direction(current_node,next_node):
+    '''This function returns the orientation in which the robot has to be rotated based on current and next node'''
     global right
     global up
     global left
@@ -170,8 +162,9 @@ def next_direction(current_node,next_node):
         print("Invalid/Inbetween oriented value")
 
 def conversion_between_node_and_position(length_of_arena,division=True):
-    if division==False:
-        length_of_arena*=2
+    '''This function returns two dictinaries with key and values as node and position values in the arena and vice versa'''
+    if division==True:
+        length_of_arena = int(length_of_arena/2)
     positions_numbers = []
     for i in range(length_of_arena-1,-1,-1):
         positions_numbers.append(-float(str(i)+".5"))
@@ -182,113 +175,129 @@ def conversion_between_node_and_position(length_of_arena,division=True):
     for z in positions_numbers:
         for x in positions_numbers:
             mid_positions.append((x,0,z))
-    for i in range(0,64):
+    for i in range(0,pow(length_of_arena,3)):
         node_to_position[i] = node_to_position.get(i,mid_positions[i])
     position_to_node = dict([(value, key) for key, value in node_to_position.items()])
     return node_to_position,position_to_node
 
-#while supervisor.step(timestep) != -1:
-length_of_arena = 4
-node_to_position,position_to_node = conversion_between_node_and_position(4)
-nodes = pow(length_of_arena,3)
-    
+#Dictinonary obtained to relate between Nodes and Positions
+(node_to_position,position_to_node) = conversion_between_node_and_position(length_of_arena)
+#print("Debug :","Node <=> Positions",node_to_position,"\n",position_to_node)
+#nodes = pow(length_of_arena,3)
+#Varible to monitor timing    
 timings = range(2,180,2)
 
-next_node1 = 0
-current_node1 = 0
-next_node2 = 0
-current_node2 = 0
-next_node3 = 0
-current_node3 = 0
-next_node4 = 0
-current_node4 = 0
+#Looping through available timings
 for index,time in enumerate(timings):
-        
+    #This While loop to holds the supervisor until required time is met    
     while supervisor.getTime() < time:
         if supervisor.step(timestep) == -1:
             quit()
+    #Code to receive messages from MYPUCK1
     if receiver1.getQueueLength()>0:
         received_data1 = receiver1.getData()
         message1 = struct.unpack("? f f ? ? ? ?",received_data1)
-        #print("Message received")
+        #Check which node the robot is present at based on input message about the robot's position
         current_node1 = position_to_node[(message1[1],0,message1[2])]
-        #print("Check =",position_to_node[(message[1],0,message[2])],(message[1],0,message[2]))
+        #print("Debug : Received Message =",message1)
+        #Update the graph for connection between nodes based on the received message
         graph_updation(position_to_node[(message1[1],0,message1[2])],message1[3],message1[4],message1[5],message1[6])
+        #Updating the Global map data
         message_to_map_converion(message1[1],message1[2],message1[3],message1[4],message1[5],message1[6])
+        #print("Debug :",Map data =",global_map_dict)
+        #Calling function to receive next node the robot has to translate to
         next_node1 = next_node_generator(current_node1,1)
-        #print("Message =",message)
         if message1[0]:
+            #Stop supervisor if target is found and reached
             quit()
-        #print("Map data =",global_map_dict)
         receiver1.nextPacket()
-        #print("Queue lenght",receiver1.getQueueLength())
+        #print("Debug :","Queue lenght",receiver1.getQueueLength())
+    
+    #Code to receive messages from MYPUCK2
     if receiver2.getQueueLength()>0:
         received_data2 = receiver2.getData()
         message2 = struct.unpack("? f f ? ? ? ?",received_data2)
-            #print("Message received")
+        #Check which node the robot is present at based on input message about the robot's position
         current_node2 = position_to_node[(message2[1],0,message2[2])]
-        #print("Check =",position_to_node[(message[1],0,message[2])],(message[1],0,message[2]))
+        #print("Debug : Received Message =",message2)
+        #Update the graph for connection between nodes based on the received message
         graph_updation(position_to_node[(message2[1],0,message2[2])],message2[3],message2[4],message2[5],message2[6])
+        #Updating the Global map data
         message_to_map_converion(message2[1],message2[2],message2[3],message2[4],message2[5],message2[6])
+        #print("Debug :",Map data =",global_map_dict)
+        #Calling function to receive next node the robot has to translate to
         next_node2 = next_node_generator(current_node2,2)
-        #print("Message =",message)
         if message2[0]:
+            #Stop supervisor if target is found and reached
             quit()
-        #print("Map data =",global_map_dict)
         receiver2.nextPacket()
-        #print("Queue lenght",receiver1.getQueueLength())
+        #print("Debug :","Queue lenght",receiver2.getQueueLength())
+    
+    #Code to receive messages from MYPUCK3
     if receiver3.getQueueLength()>0:
         received_data3 = receiver3.getData()
         message3 = struct.unpack("? f f ? ? ? ?",received_data3)
-            #print("Message received")
+        #Check which node the robot is present at based on input message about the robot's position
         current_node3 = position_to_node[(message3[1],0,message3[2])]
-        #print("Check =",position_to_node[(message[1],0,message[2])],(message[1],0,message[2]))
+        #print("Debug : Received Message =",message3)
+        #Update the graph for connection between nodes based on the received message
         graph_updation(position_to_node[(message3[1],0,message3[2])],message3[3],message3[4],message3[5],message3[6])
+        #Updating the Global map data
         message_to_map_converion(message3[1],message3[2],message3[3],message3[4],message3[5],message3[6])
+        #print("Debug :",Map data =",global_map_dict)
+        #Calling function to receive next node the robot has to translate to
         next_node3 = next_node_generator(current_node3,3)
-        #print("Message =",message)
         if message3[0]:
+            #Stop supervisor if target is found and reached
             quit()
-        #print("Map data =",global_map_dict)
         receiver3.nextPacket()
-        #print("Queue lenght",receiver1.getQueueLength())
+        #print("Debug :","Queue lenght",receiver3.getQueueLength())
+    
+    #Code to receive messages from MYPUCK4
     if receiver4.getQueueLength()>0:
         received_data4 = receiver4.getData()
         message4 = struct.unpack("? f f ? ? ? ?",received_data4)
-            #print("Message received")
+        #Check which node the robot is present at based on input message about the robot's position
         current_node4 = position_to_node[(message4[1],0,message4[2])]
-        #print("Check =",position_to_node[(message[1],0,message[2])],(message[1],0,message[2]))
+        #print("Debug : Received Message =",message4)
+        #Update the graph for connection between nodes based on the received message
         graph_updation(position_to_node[(message4[1],0,message4[2])],message4[3],message4[4],message4[5],message4[6])
+        #Updating the Global map data
         message_to_map_converion(message4[1],message4[2],message4[3],message4[4],message4[5],message4[6])
+        #print("Debug :",Map data =",global_map_dict)
+        #Calling function to receive next node the robot has to translate to
         next_node4 = next_node_generator(current_node4,4)
-        #print("Message =",message)
         if message4[0]:
+            #Stop supervisor if target is found and reached
             quit()
-        #print("Map data =",global_map_dict)
         receiver4.nextPacket()
-        #print("Queue lenght",receiver1.getQueueLength())
+        #print("Debug :","Queue lenght",receiver4.getQueueLength())
     
+    #Start changing the postions of robots only when the graph of nodes is not empty
     if bool(graph):        
-        #print("Graph",generate_edges(graph))
-        print("Graph",graph)
+        #print("Debug :","Graph =",graph)
+
+        #Debug/Monitor statements for each of the robots
+        #print("Debug : First robot","Current node =",current_node1,"Next node =",next_node1)
+        #print("Debug : Second robot","Current node =",current_node2,"Next node =",next_node2)
+        #print("Debug : Third robot","Current node =",current_node3,"Next node =",next_node3)
+        #print("Debug : Fourth robot","Current node =",current_node4,"Next node =",next_node4)
+
+        #Setting the robots to their respective next node
         trans_field1.setSFVec3f(list(node_to_position[next_node1]))
         trans_field2.setSFVec3f(list(node_to_position[next_node2]))
         trans_field3.setSFVec3f(list(node_to_position[next_node3]))
         trans_field4.setSFVec3f(list(node_to_position[next_node4]))
-        #print("Current node Robot is in =",next_node)
-            
-        #(x,y,z,angle) = rot_field.getSFRotation()
-        #print("Current orientation =",round(x),round(y),round(z),round(angle,4))
-        print("Current node =",current_node1,"Next node =",next_node1)
+        
+        #Setting the robot's orientation based on position of next node
         rot_field1.setSFRotation(list(next_direction(current_node1,next_node1)))
         rot_field2.setSFRotation(list(next_direction(current_node2,next_node2)))
         rot_field3.setSFRotation(list(next_direction(current_node3,next_node3)))
         rot_field4.setSFRotation(list(next_direction(current_node4,next_node4)))
+        
+        #Resetting Physics dependencies for each of the robots so that it doesn't behave in unexpected ways
         robot_node1.resetPhysics()
         robot_node2.resetPhysics()
         robot_node3.resetPhysics()
         robot_node4.resetPhysics()
-    
-    
-    
-   
+        
